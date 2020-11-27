bb49b2b7984a49f03ceae8847be27668b2b1070f
jerry kim <jerrykim@lucidmotors.com>
2020-11-27 10:33:44 -0800

    skip a test

--- ArxmlTable_test.go
+++ ArxmlTable_test.go
@@ -3,5 +3,6 @@ package godecoder
 import "testing"
 
 func TestArxmlLoad(t *testing.T) {
-    ArxmlLoad("/Users/jerrykim/local/resources")
+	t.SkipNow()
+	ArxmlLoad("/Users/jerrykim/local/resources")
 }

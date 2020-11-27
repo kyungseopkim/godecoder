c6b85e3d0410ef326b7a76b60b4f19efbb7b67b6
jerry kim <jerrykim@lucidmotors.com>
2020-11-27 10:03:36 -0800

    goarxml 0.0.8

--- ArxmlTable_test.go
+++ ArxmlTable_test.go
@@ -3,5 +3,5 @@ package godecoder
 import "testing"
 
 func TestArxmlLoad(t *testing.T) {
-    ArxmlLoad("/Users/jerrykim/sources/bitbuckets/message-decoder-cli/decoding/resources")
+    ArxmlLoad("/Users/jerrykim/local/resources")
 }

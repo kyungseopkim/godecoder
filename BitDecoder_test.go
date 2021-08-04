8948456939046aa2a7e627f2a1f65708da9c8185
jerry kim <jerrykim@lucidmotors.com>
2021-08-04 15:55:24 -0700

    disable

--- BitDecoder_test.go
+++ BitDecoder_test.go
@@ -1,37 +1,38 @@
 package godecoder
 
 import (
-    "fmt"
-    "github.com/kyungseopkim/goarxml"
-    "testing"
+	"fmt"
+	"testing"
+
+	"github.com/kyungseopkim/goarxml"
 )
 
 var data []byte
 
-func init()  {
-    data = []byte {82, 187, 1, 0, 0, 0, 4, 194, 1, 0, 0, 0}
+func init() {
+	data = []byte{82, 187, 1, 0, 0, 0, 4, 194, 1, 0, 0, 0}
 }
 
 func TestBitDecoder_GetValue(t *testing.T) {
-    sig := goarxml.Signal{
-        "IiBESP2_pRunout_InfoT_Chassis",
-        1,
-        59,
-        8,
-        1,
-        0,
-        250,
-        0,
-        "bar",
-        false,
-        "number",
-    }
-    decorder := NewBitDecoder(data, sig)
-    fmt.Println(decorder.GetValue())
+	sig := goarxml.Signal{
+		"IiBESP2_pRunout_InfoT_Chassis",
+		1,
+		59,
+		8,
+		1,
+		0,
+		250,
+		0,
+		"bar",
+		false,
+		"number",
+	}
+	decorder := NewBitDecoder(data, sig)
+	fmt.Println(decorder.GetValue())
 
 }
 
-func TestBitset_GetRange(t *testing.T) {
-    t.Error("hello")
+// func TestBitset_GetRange(t *testing.T) {
+//     t.Error("hello")
 
-}
+// }
